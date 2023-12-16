9be1acfcb1d0e65c7beae689f471388bbc3ee41e
param4 <parminder94171@gmail.com>
2023-12-16 14:48:23 +0530

    feat: added config and class-validator support in dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: Config.isProd(),
+      whitelist: true,
+      forbidNonWhitelisted: Config.ERROR_ON_NON_ALLOWED_KEYS,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
