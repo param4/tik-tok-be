4e58ac6b9268498aa390c51c35a05937ceed2e8a
param4 <parminder94171@gmail.com>
2024-01-07 19:03:19 +0530

    feat: adds basic user controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,10 +5,12 @@ import { User } from './user.entity';
 import { PasswordModule } from 'src/common/password/password.module';
 import { PasswordService } from 'src/common/password/password.service';
 import { BcryptService } from 'src/common/password/provider/bcrypt/bcrypt.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), PasswordModule],
   providers: [UserService, PasswordService, BcryptService],
   exports: [UserService, TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
 })
 export class UserModule {}
